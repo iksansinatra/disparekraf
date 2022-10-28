1b48c2cac6a92564fe91f7b51992d26a2cf42f80
unknown <idzanuliksan@gmail.com>
2022-10-28 14:32:28 +0800

    Fix Sedikit

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - exMultyPageApp',
-    title: 'exMultyPageApp',
+    titleTemplate: '%s - Sulawesi Tenggara',
+    title: 'Ekonomi Kreatif',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
